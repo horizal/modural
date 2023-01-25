0b791bfd17931d5e69d322ce717f258b562fe6b9
Laurent Delhorme <93991774+horizal@users.noreply.github.com>
2023-01-25 16:47:30 +0100

    Add files via upload

--- js/script copy.js
+++ js/script copy.js
@@ -1,25 +1,44 @@
+scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdddddd);
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+camera.rotation.y = 45/180*Math.PI;
+camera.position.x = 800;
+camera.position.y = 100;
+camera.position.z = 1000;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+hlight = new THREE.AmbientLight (0x404040,100);
+scene.add(hlight);
 
-camera.position.z = 5;
+directionalLight = new THREE.DirectionalLight(0xffffff,100);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
-function animate() {
-  requestAnimationFrame(animate);
+light = new THREE.PointLight(0xc4c4c4,10);
+light.position.set(0,300,500);
+scene.add(light);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+light2 = new THREE.PointLight(0xc4c4c4,10);
+light2.position.set(500,100,0);
+scene.add(light2);
 
-  renderer.render(scene, camera);
-};
+light3 = new THREE.PointLight(0xc4c4c4,10);
+light3.position.set(0,100,-500);
+scene.add(light3);
 
-animate();
+light4 = new THREE.PointLight(0xc4c4c4,10);
+light4.position.set(-500,300,500);
+scene.add(light4);
+
+let loader = new THREE.GLTFLoader();
+loader.load('scene.gltf', function(gltf){
+  car = gltf.scene.children[0];
+  car.scale.set(0.5,0.5,0.5);
+  scene.add(gltf.scene);
+  animate();
+});
